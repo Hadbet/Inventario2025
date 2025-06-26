1cac002fd4883beabba5ee4c1dc1bb7eb1ce1bce
hadbet <hadbet.altamirano@grammer.com>
2025-06-25 18:13:29 -0600

    001-Inicio

--- estaticos/navegador.php
+++ estaticos/navegador.php
@@ -3,8 +3,8 @@
 session_start();
 
 if ($_SESSION["nominaCurso"] == "" && $_SESSION["nominaCurso"]== null && $_SESSION["rol"]== "" && $_SESSION["rol"]== null) {
-    //echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=index.html'>";
-    //session_destroy();
+    echo "<META HTTP-EQUIV='REFRESH' CONTENT='1; URL=index.html'>";
+    session_destroy();
 }else{
     session_start();
     $rol =$_SESSION['rol'];

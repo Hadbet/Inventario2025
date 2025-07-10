fae87088220d9b05db2f474a7d8cc59fb567d275
hadbet <hadbet.altamirano@grammer.com>
2025-07-10 09:12:51 -0600

    002-Detalles y mejoras pequeñas

--- form_registro.php
+++ form_registro.php
@@ -1051,7 +1051,7 @@ if (strlen($nomina) == 7) {
         var marbete = parseInt(document.getElementById("scanner_input").value.split('.')[0], 10);
         var conteoM = document.getElementById("scanner_input").value.split('.')[1];
 
-        $.getJSON('https://grammermx.com/Logistica/Inventario2025/dao/consultaMarbeteCordinadores.php?marbete=' + marbete, function (data) {
+        $.getJSON('https://grammermx.com/Logistica/Inventario2025/dao/consultaMarbeteFaltantesSun.php?marbete=' + marbete, function (data) {
             for (var i = 0; i < data.data.length; i++) {
                 if (data.data[i].FolioMarbete) {
                     if (data.data[i].Estatus === '1') {

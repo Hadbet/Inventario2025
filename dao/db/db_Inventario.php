573aa58d6472a71fec27f595eb5776900b10d533
hadbet <hadbet.altamirano@grammer.com>
2025-12-04 03:26:17 -0600

    003-Implementacion de primero y segundo conteo rol de administrador

--- dao/db/db_Inventario.php
+++ dao/db/db_Inventario.php
@@ -3,9 +3,13 @@
 class LocalConector
 {
     private $host = "127.0.0.1:3306";
-    private $usuario = "u909553968_InventarioUser";
-    private $clave = "Inventario2025*";
-    private $db = "u909553968_Inventario2025";
+    //private $usuario = "u909553968_InventarioUser";
+    //private $clave = "Inventario2025*";
+    //private $db = "u909553968_Inventario2025";
+
+    private $usuario = "u909553968_inv_2025_user";
+    private $clave = "LogisticaEvidencias1.";
+    private $db = "u909553968_inv_2025_ger";
     public $conexion;
 
     public function conectar()
